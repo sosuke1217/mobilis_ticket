e2270baecac918962c7bcb991a8d9314c3054819
Yoshida Sosuke <keke@Yoshidas-MacBook-Pro.local>
2025-08-22 12:43:29 +0900

    Fix calendar imports: use individual pins and absolute import paths

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,11 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 # FullCalendar
 pin "fullcalendar", to: "fullcalendar.js", preload: true
-pin_all_from "app/javascript/calendar", under: "calendar"
+
+# Calendar modules - individual pins
+pin "calendar", to: "calendar/index.js", preload: true
+pin "calendar_core", to: "calendar/calendar_core.js", preload: true
+pin "modal", to: "calendar/modal.js", preload: true
+pin "interval_settings", to: "calendar/interval_settings.js", preload: true
+pin "reservation_form", to: "calendar/reservation_form.js", preload: true
+pin "utils", to: "calendar/utils.js", preload: true
