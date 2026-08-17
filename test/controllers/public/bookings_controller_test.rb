da5ed035f60d2b64e94866ee0414eb26b6543465
sosuke1217 <185383333+sosuke1217@users.noreply.github.com>
2026-08-17 18:18:56 +0900

    Test Google Calendar busy slot handling

--- test/controllers/public/bookings_controller_test.rb
+++ test/controllers/public/bookings_controller_test.rb
@@ -1,18 +1,59 @@
 require "test_helper"
 
-class Public::BookingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get public_bookings_new_url
-    assert_response :success
+class Public::BookingsControllerTest < ActiveSupport::TestCase
+  setup do
+    @controller = Public::BookingsController.new
+    @start_time = Time.zone.parse("2026-08-20 10:00")
+    @end_time = Time.zone.parse("2026-08-20 11:00")
   end
 
-  test "should get create" do
-    get public_bookings_create_url
-    assert_response :success
+  test "Google busy period blocks an overlapping booking slot" do
+    busy_periods = [
+      {
+        start_time: Time.zone.parse("2026-08-20 10:30"),
+        end_time: Time.zone.parse("2026-08-20 11:30")
+      }
+    ]
+
+    available = @controller.send(
+      :google_calendar_slot_available?,
+      @start_time,
+      @end_time,
+      0,
+      busy_periods
+    )
+
+    assert_not available
   end
 
-  test "should get show" do
-    get public_bookings_show_url
-    assert_response :success
+  test "Google busy period outside the reservation buffer keeps slot available" do
+    busy_periods = [
+      {
+        start_time: Time.zone.parse("2026-08-20 11:21"),
+        end_time: Time.zone.parse("2026-08-20 12:00")
+      }
+    ]
+
+    available = @controller.send(
+      :google_calendar_slot_available?,
+      @start_time,
+      @end_time,
+      20,
+      busy_periods
+    )
+
+    assert available
+  end
+
+  test "empty Google busy periods keep slot available" do
+    available = @controller.send(
+      :google_calendar_slot_available?,
+      @start_time,
+      @end_time,
+      20,
+      []
+    )
+
+    assert available
   end
 end
