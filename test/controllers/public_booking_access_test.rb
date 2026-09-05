4643f63d79494df1c665333ccf945584a78aa64a
sosuke1217 <185383333+sosuke1217@users.noreply.github.com>
2026-09-05 11:42:06 +0900

    公開予約リンクの認可テストを追加

---
+++ test/controllers/public_booking_access_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class PublicBookingAccessTest < ActionDispatch::IntegrationTest
+  setup do
+    @reservation = reservations(:one)
+    @reservation.update_columns(
+      start_time: 2.days.from_now.change(min: 0),
+      end_time: 2.days.from_now.change(min: 0) + 60.minutes,
+      status: Reservation.statuses[:tentative]
+    )
+  end
+
+  test "numeric reservation id cannot open the public page" do
+    get public_booking_path(@reservation.id)
+
+    assert_redirected_to new_public_booking_path
+  end
+
+  test "signed token opens the matching public reservation" do
+    get public_booking_path(@reservation.public_access_token)
+
+    assert_response :success
+  end
+
+  test "cancel requires a valid signed token" do
+    post cancel_public_booking_path(@reservation.id)
+    assert_redirected_to new_public_booking_path
+    assert @reservation.reload.tentative?
+
+    access_token = @reservation.public_access_token
+    post cancel_public_booking_path(access_token)
+    assert_redirected_to public_booking_path(access_token)
+    assert @reservation.reload.cancelled?
+  end
