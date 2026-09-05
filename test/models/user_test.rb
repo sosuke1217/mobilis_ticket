648d05a9b0e7ceb93569f62f66de5e27819805f0
sosuke1217 <185383333+sosuke1217@users.noreply.github.com>
2026-09-05 11:11:37 +0900

    電話番号検証を修正: test/models/user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,25 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+  test "accepts and normalizes a Japanese international phone number" do
+    user = User.new(name: "Test User", phone_number: "＋８１ ９０ー１２３４ー５６７８")
+
+    assert user.valid?
+    assert_equal "+81 90-1234-5678", user.phone_number
+  end
+
+  test "accepts a domestic phone number with hyphens" do
+    user = User.new(name: "Test User", phone_number: "090-1234-5678")
+
+    assert user.valid?
+  end
+
+  test "rejects letters in a phone number" do
+    user = User.new(name: "Test User", phone_number: "090-ABCD-5678")
+
+    assert_not user.valid?
+    assert user.errors.added?(:phone_number, :invalid, value: "090-ABCD-5678")
+  end
   # test "the truth" do
   #   assert true
   # end
