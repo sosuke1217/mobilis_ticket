9284c1f2446a88adc5b4d61cec0cf292053a8933
sosuke1217 <185383333+sosuke1217@users.noreply.github.com>
2026-09-08 13:06:43 +0900

    Add admin two-factor authentication components

---
+++ test/models/admin_user_two_factor_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class AdminUserTwoFactorTest < ActiveSupport::TestCase
+  test "OTP secret is encrypted and backup codes are single use" do
+    admin = admin_users(:one)
+    secret = TotpService.generate_secret
+    admin.otp_secret = secret
+    codes = admin.generate_backup_codes
+    admin.save!
+
+    assert_equal secret, admin.reload.otp_secret
+    assert_not_includes admin.otp_secret_ciphertext, secret
+    assert admin.consume_backup_code(codes.first)
+    assert_not admin.consume_backup_code(codes.first)
+  end
+end
